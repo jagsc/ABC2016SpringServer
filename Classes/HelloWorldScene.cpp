d2d3e72fcf429ad6ea8060ba49491a6f755acb39
NewNotMoon <new.not.moon@gmail.com>
2015-12-02 03:57:17 +0900

    add 3D rendering sample.

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -48,29 +48,33 @@ bool HelloWorld::init()
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
 
-    /////////////////////////////
-    // 3. add your codes below...
+    std::string model_ = "model/tortoise.c3b";
+    auto sprite_ = Sprite3D::create( model_ );
+    sprite_->setScale( 0.1f );
+    auto size_ = Director::getInstance()->getWinSize();
+    sprite_->setPosition( Vec2( size_.width * 4.f / 5.f, size_.height / 2.f ) );
+    addChild( sprite_ );
 
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
+    auto animation_ = Animation3D::create( model_ );
+    if( animation_ )
+    {
+        auto animate_ = Animate3D::create( animation_, 0.f, 1.933f );
+        auto swim_ = RepeatForever::create( animate_ );
+        sprite_->runAction( swim_ );
+    }
 
-    // add the label as a child to this layer
-    this->addChild(label, 1);
+    auto touch_listener_ = EventListenerTouchOneByOne::create();
+    touch_listener_->onTouchBegan = [ = ]( Touch* t_, Event* )
+    {
+        sprite_->stopActionByTag( 1 );
+        auto move_ = MoveTo::create( 2.f, t_->getLocation() );
+        move_->setTag( 1 );
+        sprite_->runAction( move_ );
+        return true;
+    };
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
+    auto dispatcher_ = Director::getInstance()->getEventDispatcher();
+    dispatcher_->addEventListenerWithSceneGraphPriority( touch_listener_, this );
     
     return true;
 }

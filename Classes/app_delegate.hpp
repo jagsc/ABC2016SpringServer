52c2a4dfb43b82edee1359e78053f65bb2044874
NewNotMoon <new.not.moon@gmail.com>
2016-02-05 21:39:15 +0900

    通信をノンブロッキング化 通信周りのコードを整理

--- Classes/app_delegate.hpp
+++ Classes/app_delegate.hpp
@@ -2,6 +2,7 @@
 #define C2XA_APP_DELEGATE_HPP
 
 #include "cocos2d.h"
+#include <c2xa/communication/winsock.hpp>
 
 /**
 @brief    The cocos2d Application.
@@ -10,6 +11,7 @@ The reason for implement as private inheritance is to hide some interface call b
 */
 class app_delegete : private cocos2d::Application
 {
+    c2xa::windows_socket_api wsa_;
 public:
     app_delegete();
     virtual ~app_delegete();
--- Classes/c2xa/scene/main_scene.cpp
+++ Classes/c2xa/scene/main_scene.cpp
@@ -4,6 +4,7 @@
     @date   created on 2015/09/24
 ****************************************************************************************/
 
+#include <c2xa/exception.hpp>
 #include <c2xa/scene/main_scene.hpp>
 #include <c2xa/communication/parse.hpp>
 #include <c2xa/sample.hpp>
@@ -14,13 +15,18 @@ using namespace c2xa::scene;
 
 
 main_scene::main_scene()
+try
     : fft_{ decltype( sample_ )::sample_size }
 {
+}
+catch( c2xa::bluetooth_exception& e )
+{
+    cocos2d::log( e.error_.message.c_str() );
+    throw e;
 }
 
 main_scene::~main_scene()
 {
-    bluetooth_server_.destroy_sock();
 }
 
 bool main_scene::init()
@@ -33,15 +39,6 @@ bool main_scene::init()
     setName( "main_scene" );
     scheduleUpdate();
 
-    if( -1 == bluetooth_server_.init_wsadata()
-     || -1 == bluetooth_server_.create_socket()
-     || -1 == bluetooth_server_.set_bluetooth_sock()
-     || -1 == bluetooth_server_.get_bluetooth_sockname()
-     || -1 == bluetooth_server_.wrap_wsa_set_service()
-     || -1 == bluetooth_server_.listen_accept_connect() )
-    {
-        return false;
-    }
 
     auto viewer_ = debug::sample_viewer::create();
     viewer_->setName( "sample_viewer" );
@@ -56,61 +53,72 @@ bool main_scene::init()
 
 void main_scene::update( float )
 {
-    std::memset( buffer_, '\0', sizeof( buffer_ ) );
-    auto ret_ = bluetooth_server_.recv_data( buffer_, sizeof( buffer_ ), 0 );
-    if( ret_ > 0 )
+    if( !connection_server_ )
     {
-        //cocos2d::log( "[BT log] %s\n", buffer_ );
-        for( auto&& i : communication::parse( buffer_ ) )
+        auto is_accept_ = listen_server_.accept();
+        if( is_accept_ )
         {
-            sample_.push( i );
+            connection_server_.reset( new bluetooth::connection_server( *is_accept_ ) );
         }
-
-        auto viewer_ = static_cast<debug::sample_viewer*>( getChildByName( "sample_viewer" ) );
-        if( viewer_ )
+    }
+    else
+    {
+        std::memset( buffer_, '\0', sizeof( buffer_ ) );
+        auto state_ = connection_server_->receive( buffer_, sizeof( buffer_ ), 0 );
+        if( state_ == bluetooth::connection_server::socket_state::success )
         {
-            viewer_->update( sample_ );
-        }
-
-        // FFT
-        //TODO: サンプル数不足の場合幅調整する
-        if( sample_.size() == sample_size )
-        {
-            double data_[ sample_size * 2 ];
-
-            auto it_ = sample_.cbegin();
-
-            for( int i = 0; i < sample_size; ++i )
+            cocos2d::log( "[BT log] %s\n", buffer_ );
+            for( auto&& i : communication::parse( buffer_ ) )
             {
-                CCASSERT( it_ != sample_.cend(), "" );
-                data_[ i * 2 ] = static_cast<double>( std::get<0>( *it_ ).x )
-                    * ( 0.54 - 0.46 * std::cos( 2 * M_PI * i / ( sample_size * 2 ) ) ); // hamming window
-                data_[ i * 2 + 1 ] = 0; // imaginary part
-                ++it_;
+                sample_.push( i );
             }
 
-            fft_.cdft( 1, data_ ); // FFT 結果はdata_に代入される
-
-            auto draw_ = static_cast<cocos2d::DrawNode*>( getChildByName( "draw_node" ) );
-            if( draw_ )
+            auto viewer_ = static_cast<debug::sample_viewer*>( getChildByName( "sample_viewer" ) );
+            if( viewer_ )
             {
-                draw_->clear();
-                int c = 0;
-                // 直流スキップ
-                for( int i = 1; i < sample_size / 2; ++i )
+                viewer_->update( sample_ );
+            }
+
+            // FFT
+            //TODO: サンプル数不足の場合幅調整する
+            if( sample_.size() == sample_size )
+            {
+                double data_[ sample_size * 2 ];
+
+                auto it_ = sample_.cbegin();
+
+                for( int i = 0; i < sample_size; ++i )
                 {
-                    draw_->drawSegment(
-                        cocos2d::Vec2{ ( c + 0.5f ) * app_width / ( sample_size + 1 ), 0 },
-                        cocos2d::Vec2{
+                    CCASSERT( it_ != sample_.cend(), "" );
+                    data_[ i * 2 ] = static_cast<double>( std::get<0>( *it_ ).x )
+                        * ( 0.54 - 0.46 * std::cos( 2 * M_PI * i / ( sample_size * 2 ) ) ); // hamming window
+                    data_[ i * 2 + 1 ] = 0; // imaginary part
+                    ++it_;
+                }
+
+                fft_.cdft( 1, data_ ); // FFT 結果はdata_に代入される
+
+                auto draw_ = static_cast<cocos2d::DrawNode*>( getChildByName( "draw_node" ) );
+                if( draw_ )
+                {
+                    draw_->clear();
+                    int c = 0;
+                    // 直流スキップ
+                    for( int i = 1; i < sample_size / 2; ++i )
+                    {
+                        draw_->drawSegment(
+                            cocos2d::Vec2{ ( c + 0.5f ) * app_width / ( sample_size + 1 ), 0 },
+                            cocos2d::Vec2{
                             ( c + 0.5f ) * app_width / ( sample_size + 1 ),
                             ( static_cast<float>( std::pow( data_[ i * 2 ], 2 ) + std::pow( data_[ i * 2 + 1 ], 2 ) ) / sample_size )
                         },
-                        app_width / sample_size / 2,
-                        cocos2d::Color4F{ 0, 0, 1, 0.5f } );
-                    ++c;
+                            app_width / sample_size / 2,
+                            cocos2d::Color4F{ 0, 0, 1, 0.5f } );
+                        ++c;
+                    }
                 }
             }
         }
+        connection_server_->send( "", sizeof( "" ), 0 );
     }
-    bluetooth_server_.send_data( "", sizeof( "" ), 0 );
 }

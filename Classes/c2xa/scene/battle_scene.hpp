49b9c561c1cfb1b8c0ca98d704b4ad3fac1e8b77
NewNotMoon <new.not.moon@gmail.com>
2016-02-09 23:45:57 +0900

    空の各シーンクラス作成

---
+++ Classes/c2xa/scene/battle_scene.hpp
@@ -0,0 +1,32 @@
+﻿
+/**
+
+    @file   battle_scene.hpp
+    @brief  battle_scene
+
+    @author 新ゝ月かりな(NewNotMoon)
+    @date   created on 2016/02/09
+
+*/
+#pragma once
+#ifndef C2XA_SCENE_BATTLE_SCENE_HPP
+#define C2XA_SCENE_BATTLE_SCENE_HPP
+
+#include <cocos2d.h>
+
+namespace c2xa
+{
+    namespace scene
+    {
+        class battle_scene
+            : public cocos2d::Scene
+        {
+        public:
+            CREATE_FUNC( battle_scene );
+            virtual bool init() override;
+            virtual void update( float ) override;
+        };
+    }
+}
+
+#endif//C2XA_SCENE_BATTLE_SCENE_HPP
---
+++ Classes/c2xa/scene/title_scene.cpp
@@ -0,0 +1,31 @@
+﻿
+/**
+
+    @file   title_scene.cpp
+    @brief  title_scene
+
+    @author 新ゝ月かりな(NewNotMoon)
+    @date   created on 2016/02/09
+
+*/
+
+#include <c2xa/scene/title_scene.hpp>
+
+using namespace c2xa::scene;
+
+bool title_scene::init()
+{
+    using namespace cocos2d;
+    if( !Scene::init() )
+    {
+        return false;
+    }
+    setName( "title_scene" );
+    scheduleUpdate();
+
+    return true;
+}
+
+void title_scene::update( float )
+{
+}
